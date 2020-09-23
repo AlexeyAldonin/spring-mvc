e41d9853821be38c0c2f020855f7296c4f670e27
AlexeyAldonin <aldoninalexey@gmail.com>
2020-09-24 00:29:51 +0300

    Исправлен выводимое количество автмобилей в соотвествии с запросом

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -14,9 +16,14 @@ import java.util.List;
 public class CarsController {
 
     @GetMapping
-    public String printCars(ModelMap model){
+    public String printCars(HttpServletRequest request, ModelMap model){
+        int count = 0;
+        String value = request.getParameter("count");
+        if (value != null) {
+            count += Integer.parseInt(value);
+        }
         CarService service = new CarService();
-        List<Car> cars = service.getCars();
+        List<Car> cars = service.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
--- 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -2,14 +2,24 @@ package web.service;
 
 import web.model.Car;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarService {
 
-    public List<Car> getCars() {
-        return Arrays.asList(new Car("Lada", 2106, true),
-                new Car("Land Rover", 4, false),
-                new Car("BMW", 6, true));
+    public List<Car> getCars(int count) {
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("Lada", 2106, true));
+        list.add(new Car("Jaguar", 351, true));
+        list.add(new Car("Land Rover", 405, false));
+        list.add(new Car("Niva", 7, true));
+        list.add(new Car("Zaporozhets", 1, true));
+        if (count > 0) {
+            int toDelete = list.size() - count;
+            for (int i = 0; i < toDelete; i++) {
+                list.remove(list.size() - 1);
+            }
+        }
+        return list;
     }
 }
